015d39cae1acd372984a1d3ea3731505aebeecc7
Henrik <henrik_mathiesen@hotmail.com>
2015-12-22 09:27:43 +0100

    -testing more koans

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,8 @@ module.exports = function(config) {
       './js/tst-koans/AboutArrays.js',
       './js/tst-koans/AboutFunctions.js',
       // We skip AboutHigherOrderFunctions.js, since it depends on _underscore
-      './js/tst-koans/AboutInheritance.js'
+      './js/tst-koans/AboutInheritance.js',
+      './js/tst-koans/AboutMutability.js'
     ]
   });
   
